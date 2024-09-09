770a54c6ba7526bd86523295b7c664cd3ec11bff
Rio <70637042+Rio0109@users.noreply.github.com>
2024-09-09 20:14:59 +0200

    Add files via upload

---
+++ vjezba16112024/Kalkulator/kalkuliraj.cs
@@ -0,0 +1,26 @@
+﻿namespace Kalkulator
+{
+    public class kalkuliraj
+    {
+        public double Zbroji(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Oduzmi(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Podjeli(double a, double b)
+        {
+            return a / b;
+        }
+
+        public double Pomnozi(double a, double b)
+        {
+            return a * b;
+        }
+
+    }
+}
---
+++ vjezba16112024/vjezba16112024/Program.cs
@@ -0,0 +1,45 @@
+﻿using Kalkulator;
+
+
+    kalkuliraj kalkuliraj = new kalkuliraj();
+
+{
+    static void Main(string[] args)
+    {
+        kalkuliraj kalkulator = new kalkuliraj();
+
+        Console.WriteLine("Unesi prvi broj: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.ReadLine();
+
+        Console.WriteLine("Unesi drugi broj: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Izaberi operaciju (+, -, *, /): ");
+        char operation = Console.ReadKey().KeyChar;
+
+        double result = 0;
+
+        switch (operation)
+        {
+            case '+':
+                result = kalkulator.Zbroji(num1, num2);
+                break;
+            case '-':
+                result = kalkulator.Oduzmi(num1, num2);
+                break;
+            case '/':
+                result = kalkulator.Podjeli(num1, num2);
+                break;
+            case '*':
+                result = kalkulator.Pomnozi(num1, num2);
+                break;
+
+            default:
+                Console.WriteLine("Nepoznata operacija.");
+                return;
+        }
+
+        Console.WriteLine($"\nRezultat: {result}");
+    }
+}
