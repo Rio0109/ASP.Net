d5edf4187223e85f25c3f99db0e756e47f5e3bb7
Rio <70637042+Rio0109@users.noreply.github.com>
2024-10-20 18:15:57 +0200

    Add files via upload
    Web api easy example

---
+++ WebApi_Rjeseno/Zadatak/Controllers/VoziloController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Zadatak.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VoziloController : Controller
+    {
+        private static List<Vozilo> vozila = new List<Vozilo>
+        {
+            new Vozilo{Id = 1, Model="Audi A4"},
+            new Vozilo{Id = 2, Model="Golf 7"},
+            new Vozilo{Id = 3, Model="BMW 3"}
+        };
+        [HttpGet]
+        public ActionResult<Vozilo> GetVozilo(int id)
+        {
+            var vozilo = vozila.FirstOrDefault(v => v.Id == id);
+            if (vozilo == null)
+            {
+                return NotFound();
+            }
+            return vozilo;
+        }
+    }
+}
---
+++ WebApi_Rjeseno/Zadatak/Vozilo.cs
@@ -0,0 +1,8 @@
+﻿namespace Zadatak
+{
+    public class Vozilo
+    {
+        public int Id {  get; set; }
+        public string Model { get; set; }
+    }
+}
